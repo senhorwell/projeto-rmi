79ea2936d1de13067d8fa1a6cc4005ef68626682
Anderson Makino <anddersonful@gmail.com>
2021-05-28 14:15:32 +0000

    Update RMIServer.java

--- src/RMIServer.java
+++ src/RMIServer.java
@@ -4,19 +4,16 @@ import java.rmi.registry.Registry;
 public class RMIServer {
 
 	Integer port = 8000;
-	Integer port2 = 8001;
 	
 	private void startServer(){
         try {
             Registry registry = LocateRegistry.createRegistry(port);
-            Registry registry2 = LocateRegistry.createRegistry(port2);
             
             registry.rebind("ConvertColor", new Grayscale());
-            registry2.rebind("ConvertColor", new Grayscale());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }      
-        System.out.println("Server ligado!!!");
+        System.out.println("Server 1 ligado!!!");
     }
 	
 	public static void main(String[] args) {
